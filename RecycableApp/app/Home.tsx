232bdcf743c55333bba67e2c53bd0990ee27acc6
VoicelessBark44 <34280572+VoicelessBark44@users.noreply.github.com>
2024-03-12 16:29:42 -0700

    Added some styling for HomeScreen
    title

--- RecycableApp/app/Home.tsx
+++ RecycableApp/app/Home.tsx
@@ -1,5 +1,5 @@
-import { Pressable, Text, View } from "react-native";
-import { TouchableOpacity, GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Pressable, Text, View, StyleSheet} from "react-native";
+import { TouchableOpacity, GestureHandlerRootView} from 'react-native-gesture-handler';
 import { useNavigation } from "@react-navigation/native";
 
 function Home(){
@@ -12,22 +12,22 @@ function Home(){
     }
 
     var ProfileButton = 
-    <TouchableOpacity onPress={() => navigation.navigate("Profile", { userData })}>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Profile", { userData })}>
         <Text>Profile</Text>
     </TouchableOpacity>
 
     var LoginButton = 
-    <TouchableOpacity onPress={() => navigation.navigate("Login", { userData })}>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login", { userData })}>
         <Text>Login</Text>
     </TouchableOpacity>
 
     var TrackingButton = 
-    <TouchableOpacity onPress={() => navigation.navigate("Tracking", { userData })}>
+    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Tracking", { userData })}>
         <Text>Tracking</Text>
     </TouchableOpacity>
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text>This is the Home Screen.</Text>
             <GestureHandlerRootView>
                 {ProfileButton}
@@ -37,5 +37,21 @@ function Home(){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#bfefff",
+    },
+    button: {
+        backgroundColor: "lightblue",
+        padding: 10,
+        margin: 10,
+        borderRadius: 8,
+        textAlign: 'center',
+    },
+});
 
 export default Home;
